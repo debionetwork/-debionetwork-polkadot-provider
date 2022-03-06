750a806a6629025122b58f14077a5daf19f059b1
Agustinus Theodorus <37565691+agustinustheo@users.noreply.github.com>
2022-03-06 23:08:03 +0700

    feat: add new lab queries. (#16)
    * feat: add new lab queries.
    
    * tests: lab queries unit tests.

--- test/unit/query/labs/labs.query.mock.ts
+++ test/unit/query/labs/labs.query.mock.ts
@@ -1,7 +1,20 @@
-import { returnToHumanMockWithParam } from "../../@polkadot-types.mock";
+import { returnToHumanMockWithParam, toHumanMock } from "../../@polkadot-types.mock";
+import { mockFunction } from "../../mock";
 
 export const labs = {
   labs(param) {
     return returnToHumanMockWithParam(param);
+  },
+  labsByCountryRegionCity(COUNTRY_ID, REGION_ID) {
+    return returnToHumanMockWithParam({COUNTRY_ID, REGION_ID});
+  },
+  labCount() {
+    return toHumanMock;
+  },
+  labCountByCountryRegionCity(COUNTRY_ID, REGION_ID) {
+    return returnToHumanMockWithParam({COUNTRY_ID, REGION_ID});
+  },
+  adminKey() {
+    return mockFunction();
   },
 }
