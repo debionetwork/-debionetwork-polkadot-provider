2f6bdb760c9e2b72e43b8f80018e40bb823fd54c
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-03-06 23:38:34 +0700

    tests: service request gas fee info unit tests.

--- test/unit/command/service-request/service-request.command.mock.ts
+++ test/unit/command/service-request/service-request.command.mock.ts
@@ -1,7 +1,13 @@
-import { signAndSend } from "../../@polkadot-api.mock";
+import { signAndSendWithPaymentInfo } from "../../@polkadot-api.mock";
 
 export const serviceRequest = {
-  retrieveUnstakedAmount(params) {
-    return signAndSend;
+  createRequest(param1, param2, param3, param4, param5) { // eslint-disable-line
+    return signAndSendWithPaymentInfo;
+  },
+  unstake(params) { // eslint-disable-line
+    return signAndSendWithPaymentInfo;
+  },
+  retrieveUnstakedAmount(params) { // eslint-disable-line
+    return signAndSendWithPaymentInfo;
   }
 }
