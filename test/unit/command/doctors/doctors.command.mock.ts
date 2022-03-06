861b9c4784a03de6657ee5759a121e4413285a81
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-03-07 00:21:23 +0700

    tests: doctor commands unit tests.

---
+++ test/unit/command/doctors/doctors.command.mock.ts
@@ -0,0 +1,13 @@
+import { signAndSend } from "../../@polkadot-api.mock";
+
+export const doctors = {
+  registerDoctor(param) { // eslint-disable-line
+    return signAndSend;
+  },
+  updateDoctor(param) { // eslint-disable-line
+    return signAndSend;
+  },
+  deregisterDoctor() { // eslint-disable-line
+    return signAndSend;
+  },
+}
