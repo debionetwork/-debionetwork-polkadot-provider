5ffb12c35e7d50fe400d4baaeacdbfe5e827ff0c
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-03-06 23:39:19 +0700

    feat: set eth adress for user profile.

--- src/command/user-profile.ts
+++ src/command/user-profile.ts
@@ -4,6 +4,18 @@ import { successCallback } from '..';
 export async function setEthAddress(
   api: ApiPromise,
   pair: any,
+  ethAddress: string,
+  callback?: () => void,
+): Promise<void> {
+  const unsub = await api.tx.userProfile
+    .setEthAddress(ethAddress)
+    .signAndSend(pair, { nonce: -1 }, ({ events, status }) => successCallback(api, { events, status, callback }));
+  unsub();
+}
+
+export async function adminSetEthAddress(
+  api: ApiPromise,
+  pair: any,
   substrateAddress: string,
   ethAddress: string,
   callback?: () => void,
