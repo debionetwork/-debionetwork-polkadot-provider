1e055f446af287cb8a968ca0cc3070731b2ce2a7
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-03-06 22:16:49 +0700

    feat: genetic analysis queries.

---
+++ src/query/genetic-analysts/genetic-analysis.ts
@@ -0,0 +1,33 @@
+import { ApiPromise } from "@polkadot/api";
+import { GeneticAnalysis } from "../../models/genetic-analysts/genetic-analysis";
+
+export async function queryGeneticAnalysisById(api: ApiPromise, geneticAnalysisId: string): Promise<GeneticAnalysis> {
+  const res = (await api.query.geneticAnalysis.geneticAnalysisById(geneticAnalysisId)).toHuman();
+  return new GeneticAnalysis(res);
+}
+
+export async function queryGeneticAnalysisByOwnerId(
+  api: ApiPromise,
+  accountId: string,
+): Promise<GeneticAnalysis[]> {
+  const geneticAnalysisIds: any = (await api.query.geneticAnalysis.geneticAnalysisByOwnerId(accountId)).toHuman();
+  const geneticAnalysisList: GeneticAnalysis[] = new Array<GeneticAnalysis>();
+  for (const geneticAnalysisId of geneticAnalysisIds) {
+    geneticAnalysisList.push(await queryGeneticAnalysisById(api, geneticAnalysisId));
+  }
+  return geneticAnalysisList;
+}
+
+export async function queryGeneticAnalysisByGeneticAnalystId(
+  api: ApiPromise,
+  accountId: string,
+): Promise<GeneticAnalysis[]> {
+  const geneticAnalysisIds: any = (
+    await api.query.geneticAnalysis.geneticAnalysisByGeneticAnalystId(accountId)
+  ).toHuman();
+  const geneticAnalysisList: GeneticAnalysis[] = new Array<GeneticAnalysis>();
+  for (const geneticAnalysisId of geneticAnalysisIds) {
+    geneticAnalysisList.push(await queryGeneticAnalysisById(api, geneticAnalysisId));
+  }
+  return geneticAnalysisList;
+}
