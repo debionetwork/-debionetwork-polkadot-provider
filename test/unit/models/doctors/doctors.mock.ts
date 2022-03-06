6fc3565b814c2dd175b1fc6e969baeb77ce30960
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-03-07 00:18:11 +0700

    tests: doctor query unit tests.

---
+++ test/unit/models/doctors/doctors.mock.ts
@@ -0,0 +1,17 @@
+export const doctorDataMock = {
+  accountId: "ACCOUNT_ID",
+  certifications: [
+    "ID"
+  ],
+  info: {
+    name: "string",
+    email: "string",
+    country: "string",
+    region: "string",
+    city: "string",
+    address: "string",
+    latitude: "string",
+    longitude: "string",
+    profileImage: "string",
+  }  
+}
