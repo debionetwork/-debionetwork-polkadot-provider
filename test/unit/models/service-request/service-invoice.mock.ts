894372e7ee67b8847a461bc0991d07a44c049ed3
unknown <jackyrahman14@gmail.com>
2022-03-07 13:45:37 +0700

    tests: service request queries

---
+++ test/unit/models/service-request/service-invoice.mock.ts
@@ -0,0 +1,11 @@
+export const serviceInvoiceDataMock = {
+    customerAddress: 'string',
+    dnaSampleTrackingId: 'string',
+    orderId: 'string',
+    payAmount: 'string',
+    qcPrice: 'string',
+    requestHash: 'string',
+    sellerAddress: 'string',
+    serviceId: 'string',
+    testingPrice: 'string',
+}
