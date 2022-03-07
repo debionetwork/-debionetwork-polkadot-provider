c09ced1cfdf6e239882039536338c5336fd449ad
unknown <jackyrahman14@gmail.com>
2022-03-08 04:33:31 +0700

    tests: unit test for genetic testing{

---
+++ test/unit/command/labs/genetic-testing/genetic-testing.command.mock.ts
@@ -0,0 +1,19 @@
+import { signAndSendWithPaymentInfo } from "../../../@polkadot-api.mock";
+
+export const geneticTesting = {
+  processDnaSample(param) {
+    return signAndSendWithPaymentInfo;
+  },
+  receiveDnaSample(param) {
+    return signAndSendWithPaymentInfo;
+  },
+  rejectDnaSample(param) {
+    return signAndSendWithPaymentInfo;
+  },
+  submitIndependentTestResult(param) {
+    return signAndSendWithPaymentInfo;
+  },
+  submitTestResult(param) {
+    return signAndSendWithPaymentInfo;
+  }
+}
