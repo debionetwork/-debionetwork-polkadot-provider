acbcfe8ed6a0f9a7094aff7e2412a2824cd85367
Kirisu <56469224+rubenkristian@users.noreply.github.com>
2022-03-11 10:29:06 +0700

    feat: hospitals query and unit test (#23)

---
+++ src/models/hospitals/hospital-info.ts
@@ -0,0 +1,11 @@
+export class HospitalInfo {
+  name: string;
+  email: string;
+  country: string;
+  region: string;
+  city: string;
+  address: string;
+  latitude?: string;
+  longitude?: string;
+  profileImage?: string;
+}
