f4336526f9502f1e5cdd0615def438f93c9d479d
JackyRahman <78291196+JackyRahman@users.noreply.github.com>
2022-03-11 18:00:10 +0700

    feat: genetic analysis orders and services (#24)
    * tests: service request queries
    
    * tests: unit test for genetic testing{
    
    * feat: create GA service model and function
    
    * tidy up import file
    
    * tests: update genetic analisys order unit testing
    
    * feat: create model for GA qualification
    
    * feat: create command for GA qualification
    
    * test: create mock GA service
    
    * tidyup indentation
    
    * tidyup indentation
    
    * bugFix: update command create order
    
    Co-authored-by: Agustinus Theodorus <37565691+agustinustheo@users.noreply.github.com>

--- test/unit/command/genetic-analyst/genetic-analysis-orders/genetic-analysis-orders.command.spec.ts
+++ test/unit/command/genetic-analyst/genetic-analysis-orders/genetic-analysis-orders.command.spec.ts
@@ -1,4 +1,12 @@
-import { setGeneticAnalysisOrderPaid, setGeneticAnalysisOrderRefunded, setGeneticAnalysisOrderFulfilled } from "../../../../../src/command/genetic-analyst";
+import {
+  cancelGeneticAnalysisOrder,
+  createGeneticAnalysisOrder,
+  setGeneticAnalysisOrderPaid,
+  setGeneticAnalysisOrderRefunded,
+  setGeneticAnalysisOrderFulfilled,
+  sudoUpdateEscrowKey,
+  updateEscrowKey
+} from "../../../../../src/command/genetic-analyst";
 import { successCallback } from "../../../../../src/index";
 import { ApiPromise, eventAndStatusMock, signAndSend } from "../../../@polkadot-api.mock";
 import { mockFunction } from "../../../mock";
@@ -19,19 +27,93 @@ describe('Genetic Analysis Orders Commands Unit Testing', () => {
   };
 
   const signAndSendSpy = jest.spyOn(signAndSend, 'signAndSend');
+  const cancelGeneticAnalysisOrderSpy = jest.spyOn(geneticAnalysisOrders, 'cancelGeneticAnalysisOrder');
+  const createGeneticAnalysisOrderSpy = jest.spyOn(geneticAnalysisOrders, 'createGeneticAnalysisOrder');
   const setGeneticAnalysisOrderPaidSpy = jest.spyOn(geneticAnalysisOrders, 'setGeneticAnalysisOrderPaid');
   const setGeneticAnalysisOrderRefundedSpy = jest.spyOn(geneticAnalysisOrders, 'setGeneticAnalysisOrderRefunded');
   const fulfillGeneticAnalysisOrderSpy = jest.spyOn(geneticAnalysisOrders, 'fulfillGeneticAnalysisOrder');
+  const sudoUpdateEscrowKeySpy = jest.spyOn(geneticAnalysisOrders, 'sudoUpdateEscrowKey');
+  const updateEscrowKeySpy = jest.spyOn(geneticAnalysisOrders, 'updateEscrowKey');
 
   beforeEach(() => {
     (mockFunction as jest.Mock).mockClear();
     (successCallback as jest.Mock).mockClear();
     signAndSendSpy.mockClear();
+    cancelGeneticAnalysisOrderSpy.mockClear();
+    createGeneticAnalysisOrderSpy.mockClear();
     setGeneticAnalysisOrderPaidSpy.mockClear();
     setGeneticAnalysisOrderRefundedSpy.mockClear();
     fulfillGeneticAnalysisOrderSpy.mockClear();
+    sudoUpdateEscrowKeySpy.mockClear();
+    updateEscrowKeySpy.mockClear();
   });
   
+  it('cancelGeneticAnalysisOrder should return', async () => {
+    // Arrange
+    const PAIR = "PAIR";
+    const ORDER_ID = "ORDER_ID";
+
+    // Act
+    await cancelGeneticAnalysisOrder(
+      API_PROMISE_MOCK as any, 
+      PAIR,
+      ORDER_ID,
+      mockFunction
+    );
+
+    expect(cancelGeneticAnalysisOrderSpy).toBeCalledTimes(1);
+    expect(cancelGeneticAnalysisOrderSpy).toBeCalledWith(ORDER_ID);
+    expect(signAndSendSpy).toBeCalledTimes(1);
+    expect(signAndSendSpy).toBeCalledWith(PAIR, { nonce: -1 }, expect.any(Function));
+    expect(successCallback).toBeCalledTimes(1);
+    expect(successCallback).toBeCalledWith(API_PROMISE_MOCK, { 
+      events: eventAndStatusMock.events, 
+      status: eventAndStatusMock.status, 
+      callback: mockFunction,
+    });
+    expect(mockFunction).toBeCalledTimes(2);
+  });
+
+  it('createGeneticAnalysisOrder should return', async () => {
+    // Arrange
+    const PAIR = "PAIR";
+    const GENETIC_DATA_ID = "GENETIC_DATA_ID";
+    const SERVICE_ID = "SERVICE_ID";
+    const PRICE_INDEX = 0;
+    const GENETIC_LINK = "GENETIC_LINK";
+    const CUSTOMER_BOX_PUBLIC_KEY = "CUSTOMER_BOX_PUBLIC_KEY";
+
+    // Act
+    await createGeneticAnalysisOrder(
+      API_PROMISE_MOCK as any, 
+      PAIR,
+      GENETIC_DATA_ID,
+      SERVICE_ID,
+      PRICE_INDEX,
+      GENETIC_LINK,
+      CUSTOMER_BOX_PUBLIC_KEY,
+      mockFunction
+    );
+
+    expect(createGeneticAnalysisOrderSpy).toBeCalledTimes(1);
+    expect(createGeneticAnalysisOrderSpy).toBeCalledWith(
+      GENETIC_DATA_ID,
+      SERVICE_ID,
+      PRICE_INDEX,
+      CUSTOMER_BOX_PUBLIC_KEY,
+      GENETIC_LINK,
+      );
+    expect(signAndSendSpy).toBeCalledTimes(1);
+    expect(signAndSendSpy).toBeCalledWith(PAIR, { nonce: -1 }, expect.any(Function));
+    expect(successCallback).toBeCalledTimes(1);
+    expect(successCallback).toBeCalledWith(API_PROMISE_MOCK, { 
+      events: eventAndStatusMock.events, 
+      status: eventAndStatusMock.status, 
+      callback: mockFunction,
+    });
+    expect(mockFunction).toBeCalledTimes(2);
+  });
+
   it('setGeneticAnalysisOrderRefunded should return', async () => {
       // Arrange
       const PAIR = "PAIR";
@@ -108,5 +190,57 @@ describe('Genetic Analysis Orders Commands Unit Testing', () => {
         callback: mockFunction,
       });
       expect(mockFunction).toBeCalledTimes(2);
+  });
+
+  it('sudoUpdateEscrowKey should return', async () => {
+    // Arrange
+    const PAIR = "PAIR";
+    const CUSTOMER_ID = "CUSTOMER_ID";
+
+    // Act
+    await sudoUpdateEscrowKey(
+      API_PROMISE_MOCK as any, 
+      PAIR,
+      CUSTOMER_ID,
+      mockFunction
+    );
+
+    expect(sudoUpdateEscrowKeySpy).toBeCalledTimes(1);
+    expect(sudoUpdateEscrowKeySpy).toBeCalledWith(CUSTOMER_ID);
+    expect(signAndSendSpy).toBeCalledTimes(1);
+    expect(signAndSendSpy).toBeCalledWith(PAIR, { nonce: -1 }, expect.any(Function));
+    expect(successCallback).toBeCalledTimes(1);
+    expect(successCallback).toBeCalledWith(API_PROMISE_MOCK, { 
+      events: eventAndStatusMock.events, 
+      status: eventAndStatusMock.status, 
+      callback: mockFunction,
+    });
+    expect(mockFunction).toBeCalledTimes(2);
+  });
+
+  it('updateEscrowKey should return', async () => {
+    // Arrange
+    const PAIR = "PAIR";
+    const CUSTOMER_ID = "CUSTOMER_ID";
+
+    // Act
+    await updateEscrowKey(
+      API_PROMISE_MOCK as any, 
+      PAIR,
+      CUSTOMER_ID,
+      mockFunction
+    );
+
+    expect(updateEscrowKeySpy).toBeCalledTimes(1);
+    expect(updateEscrowKeySpy).toBeCalledWith(CUSTOMER_ID);
+    expect(signAndSendSpy).toBeCalledTimes(1);
+    expect(signAndSendSpy).toBeCalledWith(PAIR, { nonce: -1 }, expect.any(Function));
+    expect(successCallback).toBeCalledTimes(1);
+    expect(successCallback).toBeCalledWith(API_PROMISE_MOCK, { 
+      events: eventAndStatusMock.events, 
+      status: eventAndStatusMock.status, 
+      callback: mockFunction,
+    });
+    expect(mockFunction).toBeCalledTimes(2);
   });
 })
