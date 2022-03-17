9dc0670aa0dc80da6ecf18303dfd2bd441d54029
rubenkristian <rubenkristian96@gmail.com>
2022-03-17 13:01:56 +0700

    update genetic analysis order and unit test

--- src/query/labs/orders.ts
+++ src/query/labs/orders.ts
@@ -9,3 +9,27 @@ export async function queryOrderDetailByOrderID(api: ApiPromise, orderID: string
   const res = (await api.query.orders.orders(orderID)).toHuman();
   return new Order(res);
 }
+
+export async function queryOrdersByCustomer(api: ApiPromise, customerId: string): Promise<Order[]> {
+  const ids = (await api.query.orders.ordersByCustomer(customerId)).toHuman() as Array<string>;
+  const orders: Array<Order> = new Array<Order>();
+
+  for (let i = 0; i < ids.length; i++) {
+    const order = await queryOrderDetailByOrderID(api, ids[i]);
+    orders.push(order);
+  }
+
+  return orders;
+}
+
+export async function queryOrdersBySeller(api: ApiPromise, sellerId: string): Promise<Order[]> {
+  const ids = (await api.query.orders.ordersBySeller(sellerId)).toHuman() as Array<string>;
+  const orders: Array<Order> = new Array<Order>();
+
+  for (let i = 0; i < ids.length; i++) {
+    const order = await queryOrderDetailByOrderID(api, ids[i]);
+    orders.push(order);
+  }
+
+  return orders;
+}
--- test/unit/query/labs/orders/orders.query.spec.ts
+++ test/unit/query/labs/orders/orders.query.spec.ts
@@ -1,9 +1,10 @@
 import { Order } from "../../../../../src/models/labs/orders";
-import { queryLastOrderHashByCustomer, queryOrderDetailByOrderID } from "../../../../../src/query/labs/orders";
+import { queryLastOrderHashByCustomer, queryOrderDetailByOrderID, queryOrdersByCustomer, queryOrdersBySeller } from "../../../../../src/query/labs/orders";
 import { ApiPromise } from "../../../@polkadot-api.mock";
 import { mockFunction } from "../../../mock";
 import { orderDataMock } from "../../../models/labs/orders.mock";
 import { orders } from "./orders.query.mock";
+import { when } from 'jest-when';
 
 jest.mock('../../../mock', () => ({
   mockFunction: jest.fn(),
@@ -17,11 +18,15 @@ describe('Orders Queries Unit Tests', () => {
   
   const lastOrderByCustomerSpy = jest.spyOn(orders, 'lastOrderByCustomer');
   const ordersSpy = jest.spyOn(orders, 'orders');
+  const ordersBySellerSpy = jest.spyOn(orders, "ordersBySeller");
+  const ordersByCustomerSpy = jest.spyOn(orders, "ordersByCustomer");
   
   beforeEach(() => {
     (mockFunction as jest.Mock).mockClear();
     lastOrderByCustomerSpy.mockClear();
     ordersSpy.mockClear();
+    ordersBySellerSpy.mockClear();
+    ordersByCustomerSpy.mockClear();
   });
 
   it('lastOrderByCustomer should return', async () => {
@@ -50,6 +55,56 @@ describe('Orders Queries Unit Tests', () => {
       .toEqual(EXPECTED_VALUE);
     expect(mockFunction).toBeCalledTimes(1);
     expect(mockFunction).toBeCalledWith(ORDER_ID);
+    expect(ordersSpy).toBeCalledTimes(1);
+    expect(ordersSpy).toBeCalledWith(ORDER_ID);
+  });
+
+  it('queryOrdersByCustomer should return', async () => {
+    // Arrange
+    const CUSTOMER_ID = "CUSTOMER_ID";
+    const ORDER_ID = "ORDER_ID";
+    const ORDER_IDS = [ ORDER_ID ];
+    const EXPECTED_VALUE = new Order(orderDataMock);
+
+    when(mockFunction)
+      .calledWith(CUSTOMER_ID)
+      .mockReturnValue(ORDER_IDS);
+    when(mockFunction)
+      .calledWith(ORDER_ID)
+      .mockReturnValue(EXPECTED_VALUE);
+
+    // Assert
+    expect(await queryOrdersByCustomer(API_PROMISE_MOCK as any, CUSTOMER_ID))
+      .toEqual([EXPECTED_VALUE]);
+    expect(mockFunction).toBeCalledTimes(2);
+    expect(mockFunction).toBeCalledWith(CUSTOMER_ID);
+    expect(ordersByCustomerSpy).toBeCalledTimes(1);
+    expect(ordersByCustomerSpy).toBeCalledWith(CUSTOMER_ID);
+    expect(ordersSpy).toBeCalledTimes(1);
+    expect(ordersSpy).toBeCalledWith(ORDER_ID);
+  });
+
+  it('queryOrdersBySeller should return', async () => {
+    // Arrange
+    const SELLER_ID = "SELLER_ID";
+    const ORDER_ID = "ORDER_ID";
+    const ORDER_IDS = [ ORDER_ID ];
+    const EXPECTED_VALUE = new Order(orderDataMock);
+
+    when(mockFunction)
+      .calledWith(SELLER_ID)
+      .mockReturnValue(ORDER_IDS);
+    when(mockFunction)
+      .calledWith(ORDER_ID)
+      .mockReturnValue(EXPECTED_VALUE);
+
+    // Assert
+    expect(await queryOrdersBySeller(API_PROMISE_MOCK as any, SELLER_ID))
+      .toEqual([EXPECTED_VALUE]);
+    expect(mockFunction).toBeCalledTimes(2);
+    expect(mockFunction).toBeCalledWith(SELLER_ID);
+    expect(ordersBySellerSpy).toBeCalledTimes(1);
+    expect(ordersBySellerSpy).toBeCalledWith(SELLER_ID);
     expect(ordersSpy).toBeCalledTimes(1);
     expect(ordersSpy).toBeCalledWith(ORDER_ID);
   });
