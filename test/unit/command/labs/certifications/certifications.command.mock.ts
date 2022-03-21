228f216449cbffb354886d172ce682ecaaa5ca6b
unknown <jackyrahman14@gmail.com>
2022-03-21 09:05:35 +0700

    tests: unit test labs certification

---
+++ test/unit/command/labs/certifications/certifications.command.mock.ts
@@ -0,0 +1,13 @@
+import { signAndSendWithPaymentInfo } from "../../../@polkadot-api.mock";
+
+export const certifications = {
+  createCertification(params) {
+    return signAndSendWithPaymentInfo;
+  },
+  updateCertification(params) {
+    return signAndSendWithPaymentInfo;
+  },
+  deleteCertification(params) {
+    return signAndSendWithPaymentInfo;
+  }
+}
