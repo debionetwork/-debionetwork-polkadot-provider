56e004ca32a88580b93004be0612edeedbcac5c2
Agustinus Theodorus <37565691+agustinustheo@users.noreply.github.com>
2022-04-04 13:07:10 +0700

    feat: genetic analyst service command fees and new labs extrinsics. (#46)
    * feat: genetic analyst service fees
    
    * feat: update labs pallet

--- src/command/labs/index.ts
+++ src/command/labs/index.ts
@@ -37,6 +37,52 @@ export async function deregisterLab(api: ApiPromise, pair: any, callback?: () =>
   });
 }
 
+export async function stakeLab(api: ApiPromise, pair: any, callback?: () => void) {
+  // tslint:disable-next-line
+  var unsub = await api.tx.labs.stakeLab().signAndSend(pair, { nonce: -1 }, ({ events, status }) => {
+    successCallback(api, { events, status, callback, unsub });
+  });
+}
+
+export async function unstakeLab(api: ApiPromise, pair: any, callback?: () => void) {
+  // tslint:disable-next-line
+  var unsub = await api.tx.labs.unstakeLab().signAndSend(pair, { nonce: -1 }, ({ events, status }) => {
+    successCallback(api, { events, status, callback, unsub });
+  });
+}
+
+export async function retrieveLabUnstakeAmount(api: ApiPromise, pair: any, accountId: string, callback?: () => void) {
+  // tslint:disable-next-line
+  var unsub = await api.tx.labs
+    .retrieveUnstakeAmount(accountId)
+    .signAndSend(pair, { nonce: -1 }, ({ events, status }) => {
+      successCallback(api, { events, status, callback, unsub });
+    });
+}
+
+export async function updateLabMinimumStakeAmount(api: ApiPromise, pair: any, minimum: number, callback?: () => void) {
+  // tslint:disable-next-line
+  var unsub = await api.tx.labs
+    .updateMinimumStakeAmount(minimum)
+    .signAndSend(pair, { nonce: -1 }, ({ events, status }) => {
+      successCallback(api, { events, status, callback, unsub });
+    });
+}
+
+export async function updateLabAdminKey(api: ApiPromise, pair: any, accountId: string, callback?: () => void) {
+  // tslint:disable-next-line
+  var unsub = await api.tx.labs.updateAdminKey(accountId).signAndSend(pair, { nonce: -1 }, ({ events, status }) => {
+    successCallback(api, { events, status, callback, unsub });
+  });
+}
+
+export async function sudoUpdateLabAdminKey(api: ApiPromise, pair: any, accountId: string, callback?: () => void) {
+  // tslint:disable-next-line
+  var unsub = await api.tx.labs.sudoUpdateAdminKey(accountId).signAndSend(pair, { nonce: -1 }, ({ events, status }) => {
+    successCallback(api, { events, status, callback, unsub });
+  });
+}
+
 export * from './certifications';
 export * from './genetic-testing';
 export * from './orders';
