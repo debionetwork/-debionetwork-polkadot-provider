0d9dfd11cc9372c92d282a790ca173e197c5ad4b
rubenkristian <rubenkristian96@gmail.com>
2022-04-06 19:31:05 +0700

    update genetic data integration test e2e

--- test/e2e/genetic-analysts/genetic-data.spec.ts
+++ test/e2e/genetic-analysts/genetic-data.spec.ts
@@ -8,6 +8,8 @@ describe('Genetic Data Pallet Integration Tests', () => {
   let api: ApiPromise;
   let pair: any;
 
+  let geneticData: GeneticData;
+
   beforeAll(async () => {
     const { api: _api, pair: _pair } = await initializeApi();
     api = _api;
@@ -28,17 +30,16 @@ describe('Genetic Data Pallet Integration Tests', () => {
       });
     });
 
-    expect((await geneticDataPromise).title).toEqual(geneticDataMock.title);
-    expect((await geneticDataPromise).description).toEqual(geneticDataMock.description);
-    expect((await geneticDataPromise).reportLink).toEqual(geneticDataMock.reportLink);
+    geneticData = await geneticDataPromise;
+    expect(geneticData.title).toEqual(geneticDataMock.title);
+    expect(geneticData.description).toEqual(geneticDataMock.description);
+    expect(geneticData.reportLink).toEqual(geneticDataMock.reportLink);
   }, 25000);
 
   it('updateGeneticData should return', async () => {
     const title = "title update";
     const description = "description update";
     const reportLink = "report link update";
-
-    const geneticData = (await queryGeneticDataByOwnerId(api, pair.address))[0];
     
     const geneticDataPromise: Promise<GeneticData> = new Promise((resolve, reject) => { // eslint-disable-line
       updateGeneticData(api, pair, geneticData.id, title, description, reportLink, () => {
@@ -49,14 +50,13 @@ describe('Genetic Data Pallet Integration Tests', () => {
       });
     });
 
-    expect((await geneticDataPromise).title).toEqual(title);
-    expect((await geneticDataPromise).description).toEqual(description);
-    expect((await geneticDataPromise).reportLink).toEqual(reportLink);
+    const geneticDataUpdated = await geneticDataPromise;
+    expect(geneticDataUpdated.title).toEqual(title);
+    expect(geneticDataUpdated.description).toEqual(description);
+    expect(geneticDataUpdated.reportLink).toEqual(reportLink);
   }, 25000);
 
   it('removeGeneticData should return', async () => {
-    const geneticData = (await queryGeneticDataByOwnerId(api, pair.address))[0];
-
     const geneticDataPromise: Promise<number> = new Promise((resolve, reject) => { // eslint-disable-line
       removeGeneticData(api, pair, geneticData.id, () => {
         queryGeneticDataByOwnerId(api, pair.address)
@@ -66,6 +66,7 @@ describe('Genetic Data Pallet Integration Tests', () => {
       });
     });
 
-    expect(await geneticDataPromise).toEqual(0);
+    const geneticDataRemoved = await geneticDataPromise;
+    expect(geneticDataRemoved).toEqual(0);
   }, 25000);
 });
