74ee47242235c56d6729c1e8618cb39252541e24
Dicki <bukandicki@gmail.com>
2022-06-28 10:11:58 +0700

    Update duplicate code

--- src/events/handlers/customer.ts
+++ src/events/handlers/customer.ts
@@ -1,16 +1,5 @@
 import { toFormatDebioCoin } from "../helpers"
-
-const defaultHandler = {
-  get: (target: any, name: string) => {
-    return Object.prototype.hasOwnProperty.call(target, name) ? target[name] : ({ dataEvent, value, valueMessage }) => {
-      const data = dataEvent[0]
-      const id = data[value]
-      const params = { number: id }
-      const wording = "for (" + data[valueMessage].substr(0, 4) + "..." + data[valueMessage].substr(data[valueMessage].length - 4) + ")"
-      return { data, id, params, wording }
-    }
-  }
-}
+import { defaultHandler } from "./defaultHandler"
 
 const handler = {
   orders: async ({ dataEvent, value, valueMessage, event }) => {
