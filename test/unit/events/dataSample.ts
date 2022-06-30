1ea1bfd83861737d332d0d1a59ac31ae012eeac3
Dicki <bukandicki@gmail.com>
2022-06-30 13:47:52 +0700

    Enhance data structure

--- test/unit/events/dataSample.ts
+++ test/unit/events/dataSample.ts
@@ -1,5 +1,5 @@
 export const sampleAddress = "sampleAddress"
-export const sampleRole = "customer"
+export const sampleRole = { customer: "customer", lab: "lab" }
 export const sampleState = {
   configEvent: {
     "role": {
@@ -14,12 +14,34 @@ export const sampleState = {
             "identity": 0
           },
           "ServiceRequestUnstaked": {
-            "message": "Your stake amount has been refunded, kindly check your balance.",
-            "value": 1,
-            "value_message": "",
+            "message": "",
+            "value": "hash",
+            "value_message": "Your stake amount has been refunded, kindly check your balance.",
             "route": "",
             "params": "",
             "identity": "requesterAddress"
+          }
+        }
+      },
+      "lab": {
+        "orders": {
+          "OrderPaid": {
+            "message": "",
+            "value": "id",
+            "value_message": "A new order",
+            "route": "lab-dashboard-process-order",
+            "params": "orderId",
+            "identity": "sellerId"
+          }
+        },
+        "services": {
+          "ServiceCreated": {
+            "message": "",
+            "value": "id",
+            "value_message": "You've successfully added your new service",
+            "route": "lab-dashboard-services-detail",
+            "params": "id",
+            "identity": "ownerId"
           }
         }
       }
@@ -52,49 +74,123 @@ export const sampleEvent = {
 export const sampleEvent2 = {
   "index": "sampleIndex",
   "data": [
-    "sampleAddress",
-    [
-      {
-        "hash": "sampleHash",
-        "requesterAddress": "sampleAddress",
-        "labAddress": null,
-        "country": "sampleCountry",
-        "region": "sampleRegion",
-        "city": "sampleCity",
-        "serviceCategory": "sampleServiceCategory",
-        "stakingAmount": "sampleStakingAmount",
-        "status": "Open",
-        "createdAt": "now",
-        "updatedAt": null,
-        "unstakedAt": null
-      }
-    ]
+    "sampleAnotherAddress",
+    {
+      "hash": "sampleHash",
+      "requesterAddress": "sampleAddress",
+      "labAddress": null,
+      "country": "sampleCountry",
+      "region": "sampleRegion",
+      "city": "sampleCity",
+      "serviceCategory": "sampleServiceCategory",
+      "stakingAmount": "sampleStakingAmount",
+      "status": "Open",
+      "createdAt": "now",
+      "updatedAt": null,
+      "unstakedAt": null
+    }
   ],
   "section": "serviceRequest",
-  "method": "ServiceRequestCreated"
+  "method": "ServiceRequestUnstaked"
 }
 
 export const sampleEvent3 = {
   "index": "sampleIndex",
   "data": [
-    "sampleAddress",
-    [
-      {
-        "hash": "sampleHash",
-        "requesterAddress": null,
-        "labAddress": null,
-        "country": "sampleCountry",
-        "region": "sampleRegion",
-        "city": "sampleCity",
-        "serviceCategory": "sampleServiceCategory",
-        "stakingAmount": "sampleStakingAmount",
-        "status": "Open",
-        "createdAt": "now",
-        "updatedAt": null,
-        "unstakedAt": null
-      }
-    ]
+    "sampleAnotherAddress",
+    {
+      "hash": "sampleHash",
+      "requesterAddress": null,
+      "labAddress": null,
+      "country": "sampleCountry",
+      "region": "sampleRegion",
+      "city": "sampleCity",
+      "serviceCategory": "sampleServiceCategory",
+      "stakingAmount": "sampleStakingAmount",
+      "status": "Open",
+      "createdAt": "now",
+      "updatedAt": null,
+      "unstakedAt": null
+    }
   ],
   "section": "serviceRequest",
   "method": "ServiceRequestUnstaked"
+}
+
+export const sampleEvent4 = {
+  "index": "0x1801",
+  "data": [
+    {
+      "id": "sampleIdHash",
+      "serviceId": "sampleServiceIdHash",
+      "customerId": "sampleCustId",
+      "customerBoxPublicKey": "sampleCustomerPublicKey",
+      "sellerId": "sampleAddress",
+      "dnaSampleTrackingId": "SampleDNA",
+      "currency": "DAI",
+      "prices": [
+        {
+          "component": "0x74657374696e675f7072696365",
+          "value": "0x00000000000000000de0b6b3a7640000"
+        }
+      ],
+      "additionalPrices": [
+        {
+          "component": "0x71635f7072696365",
+          "value": "0x00000000000000000de0b6b3a7640000"
+        }
+      ],
+      "status": "Paid",
+      "orderFlow": "RequestTest",
+      "createdAt": 1656564228000,
+      "updatedAt": 1656564408000
+    }
+  ],
+  "section": "orders",
+  "method": "OrderPaid"
+}
+
+export const sampleEvent5 = {
+  "index": "0x1500",
+  "data": [
+    {
+      "id": "sampleIdHash",
+      "ownerId": "sampleAddress",
+      "info": {
+        "name": "ServiceName",
+        "pricesByCurrency": [
+          {
+            "currency": "DAI",
+            "totalPrice": "0x00000000000000001bc16d674ec80000",
+            "priceComponents": [
+              {
+                "component": "0x74657374696e675f7072696365",
+                "value": "0x00000000000000000de0b6b3a7640000"
+              }
+            ],
+            "additionalPrices": [
+              {
+                "component": "0x71635f7072696365",
+                "value": "0x00000000000000000de0b6b3a7640000"
+              }
+            ]
+          }
+        ],
+        "expectedDuration": {
+          "duration": 1,
+          "durationType": "Days"
+        },
+        "category": "sampleCategory",
+        "description": "sampleDesc",
+        "dnaCollectionProcess": "sampleDNA",
+        "testResultSample": "sampleResult",
+        "longDescription": "sampleDesc",
+        "image": "sampleImage"
+      },
+      "serviceFlow": "RequestTest"
+    },
+    "sampleAddress"
+  ],
+  "section": "services",
+  "method": "ServiceCreated"
 }
