8cfdc0dc4ef284f7aedd5aba2019bfc32b3f2ad1
Azmi Muhammad <31072582+azmimuhammad@users.noreply.github.com>
2022-07-01 10:16:58 +0700

    feat: add unstake genetic analyst fee (#67)
    * feat: add unstakeGeneticAnalystFee function
    
    * test: add unstakeGeneticAnalystFee  e2e testing

--- test/e2e/genetic-analysts/index.spec.ts
+++ test/e2e/genetic-analysts/index.spec.ts
@@ -1,6 +1,6 @@
 import 'regenerator-runtime/runtime';
 import { ApiPromise } from "@polkadot/api";
-import { deregisterGeneticAnalyst, GeneticAnalyst, queryGeneticAnalystByAccountId, queryGeneticAnalystCount, queryGeneticAnalystMinimumStakeAmount, queryGeneticAnalystVerifierKey, registerGeneticAnalyst, retrieveGeneticAnalystUnstakeAmount, stakeGeneticAnalyst, unstakeGeneticAnalyst, updateGeneticAnalyst, updateGeneticAnalystAvailabilityStatus, updateGeneticAnalystMinimumStakeAmount, updateGeneticAnalystVerificationStatus } from "../../../src";
+import { deregisterGeneticAnalyst, GeneticAnalyst, queryGeneticAnalystByAccountId, queryGeneticAnalystCount, queryGeneticAnalystMinimumStakeAmount, queryGeneticAnalystVerifierKey, registerGeneticAnalyst, retrieveGeneticAnalystUnstakeAmount, stakeGeneticAnalyst, unstakeGeneticAnalyst, updateGeneticAnalyst, updateGeneticAnalystAvailabilityStatus, updateGeneticAnalystMinimumStakeAmount, updateGeneticAnalystVerificationStatus, unstakeGeneticAnalystFee } from "../../../src";
 import { initializeApi } from "../polkadot-init";
 import { geneticAnalystsDataMock } from '../../unit/models/genetic-analysts/genetic-analysts.mock';
 import { VerificationStatus } from '../../../src/primitives/verification-status';
@@ -156,4 +156,8 @@ describe('Genetic Analyst Pallet Integration Tests', () => {
     
     expect(await promise).toEqual(0);
   });
+
+  it('unstakeGeneticAnalystFee should return', async () => {
+    expect(await unstakeGeneticAnalystFee(api, pair)).toHaveProperty('partialFee')
+  })
 });
