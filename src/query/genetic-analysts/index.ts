8e53b37186a91ce30fb8efcaafb2f2dd41850cb7
Azmi Muhammad <azmim85@gmail.com>
2022-07-28 06:38:01 +0700

    fix: error testing

--- src/query/genetic-analysts/index.ts
+++ src/query/genetic-analysts/index.ts
@@ -26,7 +26,8 @@ export async function queryGeneticAnalystTotalStakedAmount(api: ApiPromise): Pro
 
 export async function queryGeneticAnalystMinimumStakeAmount(api: ApiPromise): Promise<number> {
   const res: any = (await api.query.geneticAnalysts.minimumStakeAmount()).toHuman();
-  const minimum: string = res.replaceAll(",", "")
+  const minimum: string = String(res).replace(/,/g, '')
+  
   return parseInt(minimum);
 }
 
