9fc202b38b62977becbc4d230208bd11b5769b19
Kirisu <56469224+rubenkristian@users.noreply.github.com>
2022-08-05 13:58:32 +0700

    feat: update data type price to bigint (#72)

--- src/models/genetic-analysts/genetic-analyst-service/prices-by-currency.ts
+++ src/models/genetic-analysts/genetic-analyst-service/prices-by-currency.ts
@@ -2,7 +2,7 @@ import { GAServicePrice } from './price';
 
 export class GAServicePriceByCurrency {
   currency: string;
-  totalPrice: number;
+  totalPrice: bigint;
   priceComponents: GAServicePrice[];
   additionalPrices?: GAServicePrice[];
 }
