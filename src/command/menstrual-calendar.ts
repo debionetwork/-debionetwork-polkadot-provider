05395cc19eb8f98eb62819334338b34d171ea18b
rubenkristian <rubenkristian96@gmail.com>
2022-11-16 11:13:00 +0700

    update model menstrual calendar cycle log and command

--- src/command/menstrual-calendar.ts
+++ src/command/menstrual-calendar.ts
@@ -1,6 +1,6 @@
 import { ApiPromise } from '@polkadot/api';
 import { successCallback, getCommandNonceAndSigner } from '../index';
-import { MenstrualCycleLogModel } from '../models/menstrual-calendar';
+import { MenstrualInfo } from '../models/menstrual-calendar';
 
 export async function addMenstrualCalendar(api: ApiPromise, pair: any, averageCycle: number, callback?: () => void) {
   // tslint:disable-next-line
@@ -30,12 +30,12 @@ export async function addMenstrualCycleLog(
   api: ApiPromise,
   pair: any,
   menstrualCalendarId: string,
-  menstrualCycleLog: MenstrualCycleLogModel,
+  menstrualInfos: Array<MenstrualInfo>,
   callback?: () => void,
 ) {
   // tslint:disable-next-line
   var unsub = await api.tx.menstrualCalendar
-    .addMenstrualCycleLog(menstrualCalendarId, menstrualCycleLog)
+    .addMenstrualCycleLog(menstrualCalendarId, menstrualInfos)
     .signAndSend(pair, getCommandNonceAndSigner(pair), ({ events, status }) => {
       successCallback(api, { events, status, callback, unsub });
     });
@@ -46,12 +46,12 @@ export async function updateMenstrualCycleLog(
   pair: any,
   menstrualCalendarId: string,
   menstrualCycleLogId: string,
-  menstrualCycleLog: MenstrualCycleLogModel,
+  menstrualInfo: MenstrualInfo,
   callback?: () => void,
 ) {
   // tslint:disable-next-line
   var unsub = await api.tx.menstrualCalendar
-    .updateMenstrualCycleLog(menstrualCalendarId, menstrualCycleLogId, menstrualCycleLog)
+    .updateMenstrualCycleLog(menstrualCalendarId, menstrualCycleLogId, menstrualInfo.date, menstrualInfo.symptoms, menstrualInfo.menstruation)
     .signAndSend(pair, getCommandNonceAndSigner(pair), ({ events, status }) => {
       successCallback(api, { events, status, callback, unsub });
     });
